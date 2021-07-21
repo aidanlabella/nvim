c5b8777fd7047bdeb7fca32887a841ac77260c6e
aidanlabella <apl1341@rit.edu>
2021-07-21 12:31:13 -0400

    fixed bug with java treesitter bug

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,6 @@ require('plugins')
 vim.cmd('source '..CONFIG_PATH..'/general/editor.vim')
 vim.cmd('source '..CONFIG_PATH..'/general/theme.vim')
 vim.cmd('source '..CONFIG_PATH..'/markup/general.vim')
+
 require('plugged')
 vim.cmd('source '..CONFIG_PATH..'/plugins/settings.vim')
