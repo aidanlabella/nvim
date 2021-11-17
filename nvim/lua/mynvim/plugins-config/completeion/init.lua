6e11feb627d69846854a2dd67e7c811f10ac9a28
aidanlabella <apl1341@rit.edu>
2021-11-17 11:53:37 -0500

    fixed annoying autocomplete preselect

--- nvim/lua/mynvim/plugins-config/completeion/init.lua
+++ nvim/lua/mynvim/plugins-config/completeion/init.lua
@@ -25,7 +25,7 @@ local lspkind = require('lspkind')
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm({ select = false }),
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
@@ -53,3 +53,4 @@ local lspkind = require('lspkind')
       { name = 'cmdline' }
     })
   })
+
