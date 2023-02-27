7485312fa88544270e377b7ba670b4ec24e420ae
Aidan LaBella <apl1341@rit.edu>
2023-02-27 17:02:19 -0500

    revert switch to lazy

--- nvim/lua/mynvim/plugins/init.lua
+++ nvim/lua/mynvim/plugins/init.lua
@@ -1,135 +1,130 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+return require('packer').startup(function()
+    -- This package manager (bootstrap)
+    use 'wbthomason/packer.nvim'
 
-require("lazy").setup({
-     {'navarasu/onedark.nvim',
+    -- Colorschemes
+    --use {'Mofiqul/dracula.nvim',
+        --as = 'dracula',
+    --}
+    use {'navarasu/onedark.nvim',
         as = 'onedark',
-     },
+    }
 
-     'dracula/vim',
+    use 'dracula/vim'
 
-     'eskilop/NorthernLights.vim',
-     'cpea2506/one_monokai.nvim',
-     'bluz71/vim-nightfly-guicolors',
-     'olimorris/onedarkpro.nvim',
-     'folke/tokyonight.nvim',
-     'nxvu699134/vn-night.nvim',
-     'mg979/vim-visual-multi',
-     'preservim/nerdcommenter',
-     'tpope/vim-fugitive',
-     'prabirshrestha/async.vim',
-     'ryanoasis/vim-devicons',
-     'majutsushi/tagbar',
-     'norcalli/nvim-colorizer.lua',
-     'junegunn/gv.vim',
-     'rbgrouleff/bclose.vim',
-     'francoiscabrol/ranger.vim',
-     'vim-syntastic/syntastic',
-     'jacoborus/tender.vim',
-     'machakann/vim-sandwich',
-     'lervag/vimtex',
-     'mhinz/vim-startify',
+    use 'eskilop/NorthernLights.vim'
+    use 'cpea2506/one_monokai.nvim'
+    use 'bluz71/vim-nightfly-guicolors'
+    use 'olimorris/onedarkpro.nvim'
+    use 'folke/tokyonight.nvim'
+    use 'nxvu699134/vn-night.nvim'
+
+    use 'mg979/vim-visual-multi'
+    use 'preservim/nerdcommenter'
+    use 'tpope/vim-fugitive'
+    use 'prabirshrestha/async.vim'
+    use 'ryanoasis/vim-devicons'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'majutsushi/tagbar'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'junegunn/gv.vim'
+    use 'rbgrouleff/bclose.vim'
+    use 'francoiscabrol/ranger.vim'
+    use 'vim-syntastic/syntastic'
+    use 'jacoborus/tender.vim'
+    use 'machakann/vim-sandwich'
+    use 'lervag/vimtex'
+    use 'mhinz/vim-startify'
 
     --Added for NVIM 0.5+:
-    -- {'akinsho/bufferline.nvim', tag = "*", dependencies = 'kyazdani42/nvim-web-devicons'}
-   --  'akinsho/bufferline.nvim'
-   --  {
+    --use {'akinsho/bufferline.nvim', tag = "*", requires = 'kyazdani42/nvim-web-devicons'}
+   -- use 'akinsho/bufferline.nvim'
+   -- use {
    --   'kdheepak/tabline.nvim',
-   --   dependencies = {
+   --   requires = {
    --       { 'hoob3rt/lualine.nvim', opt=true },
    --       {'kyazdani42/nvim-web-devicons', opt = true}
    --   }
    -- }
 
-   -- Neotree
-     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-        }
-    },
-
-     'karb94/neoscroll.nvim',
-     'sindrets/diffview.nvim',
-     'nvim-lua/plenary.nvim',
-     'neovim/nvim-lspconfig',
-     'williamboman/nvim-lsp-installer',
-     'nvim-treesitter/nvim-treesitter',
-     'nvim-lua/popup.nvim',
+    use 'karb94/neoscroll.nvim'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'sindrets/diffview.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-lua/popup.nvim'
 
     --Telescope
-     'nvim-telescope/telescope.nvim',
-     'nvim-telescope/telescope-project.nvim',
-     'kosayoda/nvim-lightbulb',
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-project.nvim'
+    use 'kosayoda/nvim-lightbulb'
 
     --Completion
-     'hrsh7th/cmp-nvim-lsp',
-     'hrsh7th/cmp-buffer',
-     'hrsh7th/cmp-path',
-     'hrsh7th/cmp-cmdline',
-     'hrsh7th/nvim-cmp',
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
 
-     {
+    use {
         'hoob3rt/lualine.nvim',
-     },
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
 
-     'folke/which-key.nvim',
-     { 'weilbith/nvim-code-action-menu', cmd = 'CodeActionMenu', },
-     'famiu/bufdelete.nvim',
-     {
+    use 'folke/which-key.nvim'
+    use { 'weilbith/nvim-code-action-menu', cmd = 'CodeActionMenu', }
+    use 'famiu/bufdelete.nvim'
+    use {
       'lewis6991/gitsigns.nvim',
-      dependencies = {
+      requires = {
         'nvim-lua/plenary.nvim'
       },
       config = function()
         require('gitsigns').setup()
       end
-     },
-     'folke/lsp-colors.nvim',
-     'windwp/nvim-autopairs',
-     'tanvirtin/monokai.nvim' ,
-     'phanviet/vim-monokai-pro',
+    }
+    use 'folke/lsp-colors.nvim'
+    use 'windwp/nvim-autopairs'
+    use {
+        'tanvirtin/monokai.nvim' ,
+    }
+    use {
+        'phanviet/vim-monokai-pro',
+    }
 
-     {
+    use {
       "folke/trouble.nvim",
-     },
+      requires = "kyazdani42/nvim-web-devicons",
+    }
 
-     {
+    use {
         "danymat/neogen",
         config = function()
             require('neogen').setup {}
         end,
-        dependencies = "nvim-treesitter/nvim-treesitter",
+        requires = "nvim-treesitter/nvim-treesitter",
         -- Uncomment next line if you want to follow only stable versions
         -- tag = "*"
-    },
+    }
 
-     'onsails/lspkind-nvim',
-     'ray-x/lsp_signature.nvim',
-     'hrsh7th/vim-vsnip',
-     'hrsh7th/vim-vsnip-integ',
-     'wfxr/minimap.vim',
-     'tpope/vim-surround',
-     'lukas-reineke/indent-blankline.nvim',
-     'chrisbra/csv.vim',
-    -- 'numtostr/FTerm.nvim'
+    use 'onsails/lspkind-nvim'
+    use 'ray-x/lsp_signature.nvim'
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/vim-vsnip-integ'
+    --use 'wfxr/minimap.vim'
+    --
+    -- Too ugly
+    --use 'p00f/nvim-ts-rainbow'
+    use 'tpope/vim-surround'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'chrisbra/csv.vim'
+    --use 'numtostr/FTerm.nvim'
 
     --Too buggy:
-    -- {'TimUntersberger/neogit',
-        --dependencies = {
+    --use {'TimUntersberger/neogit',
+        --requires = {
             --'nvim-lua/plenary.nvim',
             --'sindrets/diffview.nvim'
         --},
@@ -144,33 +139,42 @@ require("lazy").setup({
         --end
     --}
     -- Markdown
-    ({ "iamcco/markdown-preview.nvim",
+    use({ "iamcco/markdown-preview.nvim",
         run = "cd app && npm install",
         setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-        ft = { "markdown" }, }),
-     {"ellisonleao/glow.nvim", config = function() require("glow").setup() end},
+        ft = { "markdown" }, })
+    use {"ellisonleao/glow.nvim", config = function() require("glow").setup() end}
 
-     'dkarter/bullets.vim',
-     'yamatsum/nvim-cursorline',
-     'xiyaowong/nvim-transparent',
-     'ldelossa/litee.nvim',
-     'ldelossa/litee-calltree.nvim',
-     --{
-        --'akinsho/git-conflict.nvim', tag="*",
-        --config = function()
-            --require('git-conflict').setup()
-        --end
-     --},
+    use 'dkarter/bullets.vim'
+    use 'yamatsum/nvim-cursorline'
+    use 'xiyaowong/nvim-transparent'
+    use 'ldelossa/litee.nvim'
+    use 'ldelossa/litee-calltree.nvim'
+    use {
+        'akinsho/git-conflict.nvim', tag = "*",
+        config = function()
+            require('git-conflict').setup()
+        end
+    }
 
-     'tveskag/nvim-blame-line',
+    use 'tveskag/nvim-blame-line'
 
-     -- For Nvim 0.8+
-     {
+    -- For Nvim 0.8+
+    use 'SmiteshP/nvim-navic'
+    use {'nvim-neo-tree/neo-tree.nvim',
+        branch = "v2.x",
+        requires = {
+         "nvim-lua/plenary.nvim",
+         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+         "MunifTanjim/nui.nvim"}
+    }
+
+    use {
         "utilyre/barbecue.nvim",
-        dependencies = {
+        requires = {
             "neovim/nvim-lspconfig",
             "smiteshp/nvim-navic",
         }
-    },
+    }
 
-})
+end)
