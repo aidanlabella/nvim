0e7c14dd7ce54644f3dd65780308e216f2929ebe
aidanlabella <alabella@brown.edu>
2023-09-28 11:21:58 -0400

    update dashboard

--- nvim/lua/mynvim/plugins/init.lua
+++ nvim/lua/mynvim/plugins/init.lua
@@ -43,13 +43,8 @@ require("lazy").setup({
     'machakann/vim-sandwich',
     'lervag/vimtex',
     {
-        'glepnir/dashboard-nvim',
+       'glepnir/dashboard-nvim',
         event = 'VimEnter',
-        config = function()
-          require('dashboard').setup {
-            -- config
-          }
-        end,
         dependencies = {'nvim-tree/nvim-web-devicons'}
     },
 
